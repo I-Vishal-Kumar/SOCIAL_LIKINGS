8c26056f3cab63beb328549f004e2d40ecbcb645
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-06-23 15:48:01 +0530

    server

--- likings/app/hooks/useSocket.jsx
+++ likings/app/hooks/useSocket.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useRef } from "react";
 import io from "socket.io-client";
 
+const server = process.env.NEXT_PUBLIC_SERVER_URL;
+
 const useSocket = () => {
     // Use a ref to store the singleton socket instance
     const socketRef = useRef(null);
 
     // Initialize the socket only once
     if (!socketRef.current) {
-        socketRef.current = io("http://localhost:5000", {
+        socketRef.current = io(server, {
             autoConnect: false, // Prevent auto-connection
         });
     }
